ac87d257dd864bc27265d9adad0d2aa6403fe9e9
Hellnight2005 <abhijeet2005shinde.com>
2024-01-07 22:54:31 +0530

    User Data model

--- Data Modelling/models/user.models.js
+++ Data Modelling/models/user.models.js
@@ -1,5 +1,37 @@
 // Fisrt install [npm i mongooose]
-import mongooose from "mongoose"
-const UserSchema = new mongooose.Schema({})
+// Ther will some file downlod automaticaly such as:
+// Node_Modules
+// package-Lock.json
+// package.json
 
+// The basic three line of models
+// import mongooose from "mongoose"
+// const (Model-Name)Schema = new mongooose.Schema({})
+// export const (Model-Name) = new mongooose.model("(Model-Name)", (Model-Name)Schema)
 
+import mongooose, { models } from "mongoose"
+const UserSchema = new mongooose.Schema(
+    {
+        // you can use simple but if you use object the you defined more attribute in class  
+        Username: {
+            type: String,
+            require: true,
+            unique: true,
+            Lowercase: true
+        },
+        email:{
+            type: String,
+            require:true,
+            unique:true,
+            Lowercase:true
+        },
+        Password:{
+            type: String,
+            // you can add if else condition through array 
+            require: [true, "Password is in letter"]
+        }
+    },{
+        // Here timestamps is use for to recored time of User 
+        timestamps: true}
+)
+export const User = mongooose.model("User", UserSchema)
