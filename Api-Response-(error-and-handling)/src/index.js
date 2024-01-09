afa158e2b80a319a5b20a6d7d1d9236c5da6b97f
Hellnight2005 <abhijeet2005shinde.com>
2024-01-09 20:19:40 +0530

    Scorce file

---
+++ Api-Response-(error-and-handling)/src/index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv"
+// import mongoose, { connect } from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+// here we give dotenv path 
+dotenv.config({
+    path: 'C:\Users\Abhijeet\Desktop\Abhijeet\Back\back\Database-Connection\.env'
+})
+// here we call databases from index file 
+connectDB()
+
+
+
+/*
+import express from "express"
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", () => {
+            console.log("ERROR", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is Listening on  port ${process.env.PORT}`);
+        })
+    
+    } catch (error) {
+        console.error("ERROR",error);
+        throw err
+    }
+})()
+*/
